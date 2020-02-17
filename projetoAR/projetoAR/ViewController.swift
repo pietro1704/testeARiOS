1378f3ed8bf1b9549e59fadc6ff7835301c9fc4e
Pietro <pietropugliesi@icloud.com>
2020-02-17 12:32:52 -0300

    dominó tudo

--- projetoAR/projetoAR/ViewController.swift
+++ projetoAR/projetoAR/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+	@IBOutlet weak var sceneView: ARSCNView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		sceneView.delegate = self
+		sceneView.showsStatistics = true
+		let scene = SCNScene(named: "scene.scnassets/scene.scn")!
+		
+		sceneView.scene = scene
+		
+		
+		
+		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+		
+		view.addGestureRecognizer(tapGesture)
+		
+	}
+	
+	@objc func handleTap(sender:UITapGestureRecognizer){
+		let node = f
 	}
 
-
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		// Create a session configuration
+		let configuration = ARWorldTrackingConfiguration()
+		
+		// Run the view's session
+		sceneView.session.run(configuration)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		// Pause the view's session
+		sceneView.session.pause()
+	}
 }
 
